dc7066095a55885c17788f42433b7b9e5675f1e7
Akshat-cse-tech <akshatdangi333@gmail.com>
2026-06-13 13:28:02 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://complaint-system-kgmo.onrender.com/api'
 });
 
 API.interceptors.request.use((req) => {
